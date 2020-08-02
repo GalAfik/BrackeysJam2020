656e3f0ca2117e5e05080c7276765eeb4cb1f24f
Gal Afik <gal.p.afik@gmail.com>
2020-08-01 22:49:04 -0400

    Added record button color change and other button functionality.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -8,11 +9,23 @@ public class GameController : MonoBehaviour
 	public Recording[] Recordings;
 	public Transcript TranscriptLabel;
 	private float Timer;
+
+	public Button RecordButton;
+	private Color RecordButtonColor;
+	public Color RecordingColor;
+
+	private bool PlayerIsPlaying;
 	private bool PlayerIsRecording;
 
 	private void Start()
 	{
 		Timer = 0;
+
+		// Grab the initial record button color
+		if (RecordButton != null)
+		{
+			RecordButtonColor = RecordButton.GetComponent<Image>().color;
+		}
 	}
 
 	// Update is called once per frame
@@ -35,8 +48,8 @@ public class GameController : MonoBehaviour
 			}
 		}
 
-		// Count up the timer
-		Timer += Time.deltaTime;
+		// Count up the timer if the player is playing the recording
+		if (PlayerIsPlaying) Timer += Time.deltaTime;
 
 		// Set the transcript text on the UI every frame
 		TranscriptLabel.SetText(Recordings[CurrentRecording].Sentiments);
@@ -44,21 +57,57 @@ public class GameController : MonoBehaviour
 
     public void Play()
     {
-		print("play");
-    }
+		PlayerIsPlaying = true;
+	}
 
 	public void Rewind()
 	{
-		print("rewind");
+		Timer = 0;
+		PlayerIsPlaying = false;
+		PlayerIsRecording = false;
+		ResetButtonColor();
+
+		// Reset all sentiments
+		Recording.Sentiment[] Sentiments = Recordings[CurrentRecording].Sentiments;
+		foreach (var sentiment in Sentiments)
+		{
+			sentiment.Current = false;
+			sentiment.Recorded = false;
+		}
+	}
+
+	private void ResetButtonColor()
+	{
+		// Toggle record button color
+		if (RecordButton != null)
+		{
+			RecordButton.GetComponent<Image>().color = (PlayerIsRecording ? RecordingColor : RecordButtonColor);
+		}
 	}
 
 	public void Record()
 	{
+		// Toggle recording state
 		PlayerIsRecording = !PlayerIsRecording;
+
+		// Toggle record button color
+		ResetButtonColor();
 	}
 
 	public void Submit()
 	{
-		print("submit");
+		string recordedTranscript = "";
+		// Grab the current sentiments that are recorded
+		Recording.Sentiment[] Sentiments = Recordings[CurrentRecording].Sentiments;
+		foreach (var sentiment in Sentiments)
+		{
+			if (sentiment.Recorded)
+			{
+				recordedTranscript += sentiment.Phrase + " ";
+			}
+		}
+
+		// TODO
+		print(recordedTranscript);
 	}
 }
