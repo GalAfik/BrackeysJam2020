f94c5728fcec8190fde7ec724ab5a3af07c8a3e8
Gal Afik <gal.p.afik@gmail.com>
2020-08-03 00:35:25 -0400

    Added the main menu and related items. Altered the LightFixture script to sway on any axis and take the initial rotation into account.

--- Assets/Scripts/LightFixture.cs
+++ Assets/Scripts/LightFixture.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class LightFixture : MonoBehaviour
 {
+	public enum Axis { X, Y, Z };
+	public Axis SwayAxis = Axis.Z;
 	public float SwayAmount = 2f;
 	public float SwaySpeed = 0.4f;
 
-    // Update is called once per frame
-    void Update()
+	private Vector3 InitialRotation;
+
+	private void Start()
+	{
+		// Get the initial rotation to offset later
+		InitialRotation = transform.eulerAngles;
+	}
+
+	// Update is called once per frame
+	void Update()
     {
-		float zRotation = Mathf.Sin(SwaySpeed * 2 * Mathf.PI * Time.time) * SwayAmount;
-		transform.rotation = Quaternion.Euler(new Vector3(0, 0, zRotation));
+		Vector3 newRotation = InitialRotation;
+		switch (SwayAxis)
+		{
+			case Axis.X:
+				newRotation.x = InitialRotation.x + Mathf.Sin(SwaySpeed * 2 * Mathf.PI * Time.time) * SwayAmount;
+				break;
+			case Axis.Y:
+				newRotation.y = InitialRotation.y + Mathf.Sin(SwaySpeed * 2 * Mathf.PI * Time.time) * SwayAmount;
+				break;
+			default:
+				newRotation.z = InitialRotation.z + Mathf.Sin(SwaySpeed * 2 * Mathf.PI * Time.time) * SwayAmount;
+				break;
+		}
+		transform.rotation = Quaternion.Euler(newRotation);
     }
 }
