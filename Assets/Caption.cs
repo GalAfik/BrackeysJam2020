851d560eac2fbad938be554dcd66e5bf16dc8ff6
Gal Afik <gal.p.afik@gmail.com>
2020-08-04 13:55:33 -0400

    Added caption labels to each UI button

---
+++ Assets/Caption.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Caption : MonoBehaviour
+{
+	public TMP_Text CaptionLabel;
+	private Color TextColor;
+	private string Text;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Text = CaptionLabel.text;
+		TextColor = CaptionLabel.color;
+		TextColor.a = 0;
+
+		CaptionLabel?.SetText("<color=#" + ColorUtility.ToHtmlStringRGBA(TextColor) + ">" + Text + "</color>");
+	}
+
+	public void OnMouseOver()
+	{
+		TextColor.a = 1;
+		CaptionLabel?.SetText("<color=#" + ColorUtility.ToHtmlStringRGBA(TextColor) + ">" + Text + "</color>");
+	}
+
+	public void OnMouseExit()
+	{
+		TextColor.a = 0;
+		CaptionLabel?.SetText("<color=#" + ColorUtility.ToHtmlStringRGBA(TextColor) + ">" + Text + "</color>");
+	}
+}
